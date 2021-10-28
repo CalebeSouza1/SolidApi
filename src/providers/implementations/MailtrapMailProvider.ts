9ed56b534457a296b930dd2ab5cd416b3073decf
Calebe Souza <86796719+CalebeSouza1@users.noreply.github.com>
2021-10-28 17:25:46 -0300

    Add files via upload

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,34 @@
+import { IMailProvider, IMessage } from "../IMailProvider";
+import nodemailer from 'nodemailer';
+import Mail from "nodemailer/lib/mailer";
+
+
+export class MailtrapMailProvider implements IMailProvider {
+    private transporter: Mail;
+
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.mailtrap.io',
+            port: 2525,
+            auth: {
+                user: '371e8578ce968e',
+                pass: 'f05e2faa23e672'
+            }
+        })
+    }
+
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email
+            },
+            subject: message.subject,
+            html: message.body,
+        })
+    }
+}
